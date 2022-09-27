9c6a40e6dbc15a4175cf80ac6df498e8d70bbf14
428780 <robbie@vanhoof-erp.nl>
2022-09-27 14:08:34 +0200

    PlayerLookBehaviour

---
+++ Black Sails at Midnight/Assets/Player/Scripts/PlayerLookBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerLookBehaviour : MonoBehaviour
+{
+    private FirstPersonController FPController;
+    private Camera FPCamera;
+
+    // Monobahaviour Methods
+    public void Start() {
+        FPController = GetComponentInChildren<FirstPersonController>();
+        FPCamera = FPController.GetComponentInChildren<Camera>();
+    }
+
+    // Public Methods
+    public void LookAtTarget(GameObject targetOpbject)
+    {
+        RotatePlayerTowards(gameObject.transform.position);
+    }
+
+    public void LookAtPosition(Vector3 targetPosition)
+    {
+        RotatePlayerTowards(targetPosition);
+    }
+
+    // Private Methods
+    private void RotatePlayerTowards(Vector3 position)
+    {
+        Vector3 targetDirection = (position - FPCamera.transform.position).normalized;
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, FPController.transform.up);
+        FPController.m_MouseLook.m_CharacterTargetRot = new Quaternion(0, targetRotation.y, 0, targetRotation.w);
+        FPController.m_MouseLook.m_CameraTargetRot = new Quaternion(targetRotation.x, 0, 0, targetRotation.w);
+    }
+}
