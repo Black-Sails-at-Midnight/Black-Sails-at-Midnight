ead2cfff1a992f6b7cbd373d3c6739cd3ba4d41a
Nathan Thus <nathan.thus@outlook.com>
2022-09-28 15:46:29 +0200

    Finalized Multi-Ring Script

--- Black Sails at Midnight/Assets/Ring System/RingsManager.cs
+++ Black Sails at Midnight/Assets/Ring System/RingsManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class RingsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    public float Radius = 3;
+    [SerializeField]
+    public Coordinates Origin;
+    [SerializeField]
+    [Range(0, 90)]
+    public int QuarterOfPathingPoints = 6;
+    [SerializeField]
+    [Range(0, 100000)]
+    float SpaceBetweenRings = 64;
+    [SerializeField]
+    [Range(0, 100)]
+    int NumberOfRings = 6;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    List<GameObject> Rings;
+
+    [SerializeField]
+    GameObject Ring;
+
+    private void Start()
     {
-        
+        Rings = new List<GameObject>();
+
+        for (int i = 0; i < NumberOfRings; i++)
+        {
+            GameObject ring = Instantiate(Ring,transform);
+            RingSystem temp = ring.GetComponent<RingSystem>();
+            temp.Origin = Origin;
+            temp.QuarterOfPathingPoints = QuarterOfPathingPoints;
+            temp.Radius = Radius + SpaceBetweenRings * i;
+            temp.GenerateRing();
+            Rings.Add(ring);
+        }
     }
 }
