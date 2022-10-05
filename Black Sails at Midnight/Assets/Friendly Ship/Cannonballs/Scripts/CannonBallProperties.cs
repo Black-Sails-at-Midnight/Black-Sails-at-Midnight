eeac7cf6b9256a1f5f522e126ed50e294cf82e35
Nathan Thus <nathan.thus@outlook.com>
2022-10-05 09:29:42 +0200

    Made Cannonballs

--- Black Sails at Midnight/Assets/Friendly Ship/Cannonballs/Scripts/CannonBallProperties.cs
+++ Black Sails at Midnight/Assets/Friendly Ship/Cannonballs/Scripts/CannonBallProperties.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CannonBallProperties : MonoBehaviour
@@ -7,4 +9,18 @@ public class CannonBallProperties : MonoBehaviour
     [Header("Cannon Ball Properties")]
     [SerializeField]
     float Damage;
+    bool isDone;
+
+    public virtual void Effect()
+    {
+        
+    }
+
+    public bool IsEffectDone()
+    {
+        return isDone;
+    }
 }
+
+
+
