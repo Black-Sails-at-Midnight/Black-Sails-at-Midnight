d71ae249aa47b581584955209732febb45d8b608
Nathan Thus <nathan.thus@outlook.com>
2022-10-07 14:18:21 +0200

    Changed References

--- Black Sails at Midnight/Assets/Status Effects/ChainShot.cs
+++ Black Sails at Midnight/Assets/Status Effects/ChainShot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ChainShot : AttackAttributes
+public class ChainShot : CannonBallProperties
 {
         [Header("Chainshot Properties")]
         [SerializeField]
