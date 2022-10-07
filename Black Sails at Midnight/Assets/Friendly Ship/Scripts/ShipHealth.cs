64c7a58a824748032b9275cba8e94b8b1c426874
Nathan Thus <nathan.thus@outlook.com>
2022-10-07 08:35:43 +0200

    Created ShipHealth

---
+++ Black Sails at Midnight/Assets/Friendly Ship/Scripts/ShipHealth.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public float maxHealth = 100;
+    [SerializeField]
+    List<CannonBallProperties> StatusEffects;
+
+    bool isCheckingStatusEffects = false;
+
+    public float Health
+    {
+        get { return currentHealth; }
+        private set
+        {
+            currentHealth = value;
+            HealthUpdate();
+        }
+    }
+
+    private float currentHealth;
+
+    // Monobehaviour Methods
+    public void Start()
+    {
+        Health = maxHealth;
+    }
+
+    public void Update()
+    {
+        if (!isCheckingStatusEffects)
+        {
+            StartCoroutine(ApplyStatusEffects());
+        }
+    }
+
+    // Interface Implementations
+    public void Hit(GameObject source, Attack attack)
+    {
+        Health = -attack.attackSettings.damage * attack.attackSettings.damageMultiplier;
+    }
+
+    public void Hit(float damage)
+    {
+        Health = -damage;
+    }
+
+    // Public Methods
+    public void Heal(GameObject source, float amount)
+    {
+        if ((Health + amount) > maxHealth)
+            Health = maxHealth;
+        else
+            Health += amount;
+    }
+
+
+    // Private Methods
+    private void HealthUpdate()
+    {
+        if (Health < 0)
+        {
+            //TODO: Remove Enemy from play.
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator ApplyStatusEffects()
+    {
+        isCheckingStatusEffects = true;
+        foreach (var item in StatusEffects)
+        {
+            if (!item.IsEffectDone())
+            {
+                item.Effect(this);
+            }
+            else
+            {
+                StatusEffects.Remove(item);
+            }
+        }
+        yield return new WaitForSeconds(1);
+        isCheckingStatusEffects = false;
+    }
+
+}
