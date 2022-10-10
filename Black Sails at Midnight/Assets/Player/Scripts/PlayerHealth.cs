4fd98b7f88443b8d8ab9bab9dd656bae1bcb1cad
Nathan Thus <nathan.thus@outlook.com>
2022-10-10 16:22:45 +0200

    Update PlayerHealth.cs
    Fixed bug

--- Black Sails at Midnight/Assets/Player/Scripts/PlayerHealth.cs
+++ Black Sails at Midnight/Assets/Player/Scripts/PlayerHealth.cs
@@ -16,8 +16,8 @@ public class PlayerHealth : MonoBehaviour, IAttackable
         }
     }
 
-
-    private float currentHealth;
+    [SerializeField]
+    float currentHealth;
     private PlayerDeathHandler deathHandler;
 
 
@@ -31,12 +31,12 @@ public class PlayerHealth : MonoBehaviour, IAttackable
     // Interface Implementations
     public void Hit(GameObject source, Attack attack)
     {
-        Health =- attack.attackSettings.damage * attack.attackSettings.damageMultiplier;
+        Health -= attack.attackSettings.damage * attack.attackSettings.damageMultiplier;
     }
 
     public void Hit(float damage)
     {
-        Health =- damage;
+        Health -= damage;
     }
 
     // Public Methods
