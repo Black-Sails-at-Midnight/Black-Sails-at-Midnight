3c1d76c1aa6cdc960c5afa6d41977ee41d29c668
Nathan Thus <nathan.thus@outlook.com>
2022-10-31 16:22:02 +0100

    Update EnemyBullet.cs
    Works too.

--- Black Sails at Midnight/Assets/Enemy Ship/Crew/EnemyBullet.cs
+++ Black Sails at Midnight/Assets/Enemy Ship/Crew/EnemyBullet.cs
@@ -6,13 +6,14 @@ public class EnemyBullet : MonoBehaviour
 {
     [SerializeField]
     private float damage = 20;
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) //In an ideal world, where the deadline could be moved, I'd write better code. Sadly, this isn't an ideal world.
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
-            other.GetComponent<PlayerHealth>().Hit(damage);
+            //other.GetComponent<PlayerHealth>().Hit(damage);
+            Destroy(gameObject);
         }
-        else if (other.tag != "Crew")
+        else if (other.tag != "EnemyCrew" && other.tag != "Enemy" && other.tag != "Projectile")
         {
             Destroy(gameObject);
         }
