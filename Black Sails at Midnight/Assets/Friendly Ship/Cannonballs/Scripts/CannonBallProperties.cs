e1e93e1ad83ea69a8a5c96dd87e613bd236acca8
428780 <robbie@vanhoof-erp.nl>
2022-11-03 13:44:50 +0100

    Big Chungus Deafeatus Sadge

--- Black Sails at Midnight/Assets/Friendly Ship/Cannonballs/Scripts/CannonBallProperties.cs
+++ Black Sails at Midnight/Assets/Friendly Ship/Cannonballs/Scripts/CannonBallProperties.cs
@@ -24,15 +24,13 @@ public class CannonBallProperties : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<ShipHealth>() != null && other.GetType() == typeof(MeshCollider))
-        {
-            Debug.Log("Hit ship");
+        {            
             other.GetComponent<ShipHealth>().Hit(Damage);
             Destroy(this.gameObject);
         }
 
         else if (other.GetComponent<IslandHealth>() != null && this.tag == "Enemy")
-        {
-            Debug.Log("Hit island");
+        {            
             other.GetComponent<IslandHealth>().Hit(Damage);
             Destroy(this.gameObject);
         }
